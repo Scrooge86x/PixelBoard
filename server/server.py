2f711d48132e63ca8e51fd73519c32a6486ae6e2
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-12-15 17:10:32 +0100

    Add a basic websocket implementation to the server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+
+IMAGE_WIDTH = 160
+IMAGE_HEIGHT = 128
+BYTES_PER_PIXEL = 2
+IMAGE_BYTES = IMAGE_WIDTH * IMAGE_HEIGHT * BYTES_PER_PIXEL
+
+current_image = bytearray(IMAGE_BYTES)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -9,3 +16,20 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def index():
     return FileResponse("static/index.html")
+
+
+@app.websocket("/ws")
+async def websocket(ws: WebSocket):
+    await ws.accept()
+    await ws.send_bytes(current_image)
+
+    try:
+        while True:
+            data = await ws.receive_bytes()
+            if not data:
+                continue
+
+            if len(data) == IMAGE_BYTES:
+                current_image[:] = data
+    except WebSocketDisconnect:
+        pass
