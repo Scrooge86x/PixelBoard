b7c680c2d542a122aec165c1527754ad228b41bd
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-12-15 16:15:01 +0100

    Make #brush-size-value display the current value of #brush-size slider

--- server/static/app.js
+++ server/static/app.js
@@ -1,1 +1,6 @@
-alert('Hello World!');
+const g_brushSizeEl = document.querySelector('#brush-size');
+const g_brushSizeValueEl = document.querySelector('#brush-size-value');
+
+g_brushSizeEl.addEventListener('input', () => {
+    g_brushSizeValueEl.textContent = g_brushSizeEl.value;
+});
